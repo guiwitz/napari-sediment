bd61c5b1df49bc20494de2d1c25b3bad5adb3cb4
guiwitz <guiwitz@users.noreply.github.com>
2023-04-26 15:45:10 +0200

    create separate load params fun

--- src/napari_sediment/io.py
+++ src/napari_sediment/io.py
@@ -1,6 +1,8 @@
 import numpy as np
 import tifffile
 import yaml
+from pathlib import Path
+from .parameters import Param
 
 def save_mask(mask, filename):
    
@@ -22,6 +24,16 @@ def load_params_yml(params):
         setattr(params, k, documents[k])
 
     return params
+
+def load_project_params(folder):
+    """Load project parameters from yaml file in a given folder."""
+
+    folder = Path(folder)
+    params = Param(project_path=folder)
+    params = load_params_yml(params)
+    
+    return params
+
 
 def get_mask_path(export_folder):
 
