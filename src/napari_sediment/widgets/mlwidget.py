9e1224c1b5336f52f2bc2bcf7d974d73e8cacd25
guiwitz <guiwitz@users.noreply.github.com>
2023-08-18 23:55:20 +0200

    possibility to load/save ml model

--- src/napari_sediment/widgets/mlwidget.py
+++ src/napari_sediment/widgets/mlwidget.py
@@ -4,9 +4,10 @@ from qtpy.QtWidgets import (QVBoxLayout, QPushButton, QWidget,
 from qtpy.QtCore import Qt
 import numpy as np
 import skimage
+from pathlib import Path
 
 from ..classifier import Classifier
-from napari_convpaint.conv_paint_utils import Hookmodel
+from napari_convpaint.conv_paint_utils import Hookmodel, Classifier
 from napari_convpaint.conv_paint_utils import (get_features_current_layers,
                                                train_classifier, predict_image)
 
@@ -20,7 +21,7 @@ class MLWidget(QWidget):
         self.parent = parent
         self.viewer = napari_viewer
         self.pixclass = None
-        self.model = None
+        self.classifier = None
         self.data = None
 
         self.main_layout = QGridLayout()
@@ -46,6 +47,10 @@ class MLWidget(QWidget):
         self.main_layout.addWidget(self.btn_reset_mlmodel, 3, 0, 1, 2)
         self.btn_ml_mask = QPushButton("Generate mask")
         self.main_layout.addWidget(self.btn_ml_mask, 4, 0, 1, 2)
+        self.btn_save_model = QPushButton("Save model")
+        self.main_layout.addWidget(self.btn_save_model, 5, 0, 1, 2)
+        self.btn_load_model = QPushButton("Load model")
+        self.main_layout.addWidget(self.btn_load_model, 6, 0, 1, 2)
 
         self.add_connections()
 
@@ -54,6 +59,9 @@ class MLWidget(QWidget):
         self.btn_add_annotation_layer.clicked.connect(self._on_click_add_annotation_layer)
         self.btn_reset_mlmodel.clicked.connect(self._on_initialize_model)
         self.btn_ml_mask.clicked.connect(self._on_click_ml_mask)
+        self.btn_save_model.clicked.connect(self._on_click_save_model)
+        self.btn_load_model.clicked.connect(self._on_click_load_model)
+
 
     def _on_click_add_annotation_layer(self):
         """Add annotation layer to viewer"""
@@ -69,38 +77,72 @@ class MLWidget(QWidget):
             raise ValueError('No annotation layer found')
         
         reduce_fact = self.spin_downscale.value()
+        self.data = self.get_data()
+        annotations = self.viewer.layers['annotations'].data[::reduce_fact,::reduce_fact]
+        
+        if self.classifier is None:
+            self.classifier = Classifier()
+
+        features, targets = get_features_current_layers(
+            model=self.classifier.model, image=self.data, annotations=annotations,
+            scalings=self.classifier.param.scalings,
+            order=self.classifier.param.order, use_min_features=self.classifier.param.use_min_features)
+        self.classifier.random_forest = train_classifier(features, targets)
+
+    def get_data(self):
+
+        reduce_fact = self.spin_downscale.value()
         data = self.viewer.layers['imcube'].data
         if len(data) !=3:
             raise ValueError('Only three channel images are supported')
-        annotations = self.viewer.layers['annotations'].data[::reduce_fact,::reduce_fact]
+        
         if self.check_smoothing.isChecked():
             data = skimage.filters.gaussian(data, sigma=self.spin_gaussian_smoothing.value(), preserve_range=True)[:, ::4, ::4]
         else:
             data = data[:, ::reduce_fact, ::reduce_fact]
-        self.data = data.mean(axis=0)
-        
-        if self.model is None:
-            self.model = Hookmodel(model_name='vgg16')
-            self.model.register_hooks(selected_layers=[list(self.model.module_dict.keys())[0]])
-
-        features, targets = get_features_current_layers(
-            model=self.model, image=self.data, annotations=annotations, scalings=[1,2], order=1, use_min_features=False)
-        self.random_forest = train_classifier(features, targets)
-
+        return data.mean(axis=0)
 
     def _on_click_ml_mask(self):
 
-        if 'annotations' not in self.viewer.layers:
-            raise ValueError('No annotation layer found')
+        #if 'annotations' not in self.viewer.layers:
+        #    raise ValueError('No annotation layer found')
         
-        if self.model is None:
+        if self.classifier is None:
             self._on_initialize_model()
         
-        pred = predict_image(self.data, self.model, self.random_forest, scalings=[1,2], order=1, use_min_features=False)
+        if self.data is None:
+            self.data = self.get_data()
+
+        pred = predict_image(
+            self.data, self.classifier.model,
+            self.classifier.random_forest,
+            self.classifier.param.scalings,
+            self.classifier.param.order, self.classifier.param.use_min_features)
+        
         pred = (pred == 1).astype(np.uint8)
         predict_upscale = skimage.transform.resize(
-            pred, self.viewer.layers['annotations'].data.shape, order=0)
+            pred, self.viewer.layers['mask'].data.shape, order=0)
         if 'ml-mask' in self.viewer.layers:
             self.viewer.layers['ml-mask'].data = predict_upscale
         else:
             self.viewer.add_labels((predict_upscale==1).astype(np.uint8), name='ml-mask')
+
+    def _on_click_save_model(self, event=None, save_path=None):
+            
+        if save_path is None:
+            dialog = QFileDialog()
+            save_path, _ = dialog.getSaveFileName(self, "Save model", None, "JOBLIB (*.joblib)")
+            save_path = Path(save_path)
+
+        self.classifier.save_classifier(save_path)
+
+    def _on_click_load_model(self, event=None, load_path=None):
+
+        self.classifier = Classifier()
+    
+        if load_path is None:
+            dialog = QFileDialog()
+            load_path, _ = dialog.getOpenFileName(self, "Load model", None, "JOBLIB (*.joblib)")
+            load_path = Path(load_path)
+
+        self.classifier.load_model(load_path)
