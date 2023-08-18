d35e1081cd0e85a32f993049ecbe664df0f0df3d
guiwitz <guiwitz@users.noreply.github.com>
2023-08-18 17:42:01 +0200

    use convpaint classifier

--- src/napari_sediment/widgets/mlwidget.py
+++ src/napari_sediment/widgets/mlwidget.py
@@ -6,6 +6,9 @@ import numpy as np
 import skimage
 
 from ..classifier import Classifier
+from napari_convpaint.conv_paint_utils import Hookmodel
+from napari_convpaint.conv_paint_utils import (get_features_current_layers,
+                                               train_classifier, predict_image)
 
 class MLWidget(QWidget):
     """Widget for ml pixel classification. Parent should have :
@@ -17,6 +20,8 @@ class MLWidget(QWidget):
         self.parent = parent
         self.viewer = napari_viewer
         self.pixclass = None
+        self.model = None
+        self.data = None
 
         self.main_layout = QGridLayout()
         self.setLayout(self.main_layout)
@@ -31,10 +36,16 @@ class MLWidget(QWidget):
         self.spin_gaussian_smoothing.setSingleStep(0.1)
         self.spin_gaussian_smoothing.setValue(3)
         self.main_layout.addWidget(self.spin_gaussian_smoothing, 1, 1, 1, 1)
-        self.btn_reset_mlmodel = QPushButton("Reset/Initialize ML model")
-        self.main_layout.addWidget(self.btn_reset_mlmodel, 2, 0, 1, 2)
+        self.spin_downscale = QSpinBox()
+        self.spin_downscale.setRange(1, 10)
+        self.spin_downscale.setSingleStep(1)
+        self.spin_downscale.setValue(4)
+        self.main_layout.addWidget(QLabel('Downscale factor'), 2, 0, 1, 1)
+        self.main_layout.addWidget(self.spin_downscale, 2, 1, 1, 1)
+        self.btn_reset_mlmodel = QPushButton("(Re-)train pixel classifier")
+        self.main_layout.addWidget(self.btn_reset_mlmodel, 3, 0, 1, 2)
         self.btn_ml_mask = QPushButton("Generate mask")
-        self.main_layout.addWidget(self.btn_ml_mask, 3, 0, 1, 2)
+        self.main_layout.addWidget(self.btn_ml_mask, 4, 0, 1, 2)
 
         self.add_connections()
 
@@ -52,13 +63,12 @@ class MLWidget(QWidget):
             return
         self.viewer.add_labels(np.zeros_like(self.viewer.layers['mask'].data), name='annotations', opacity=0.5)
 
-    def _on_initialize_model(self):
+    def _on_initialize_model(self, event=None):
 
         if 'annotations' not in self.viewer.layers:
             raise ValueError('No annotation layer found')
         
-        reduce_fact = 4
-        #data = np.stack([self.viewer.layers[x].data for x in self.parent.rgb_names], axis=0)
+        reduce_fact = self.spin_downscale.value()
         data = self.viewer.layers['imcube'].data
         if len(data) !=3:
             raise ValueError('Only three channel images are supported')
@@ -67,26 +77,26 @@ class MLWidget(QWidget):
             data = skimage.filters.gaussian(data, sigma=self.spin_gaussian_smoothing.value(), preserve_range=True)[:, ::4, ::4]
         else:
             data = data[:, ::reduce_fact, ::reduce_fact]
-        self.pixclass = Classifier(data=data, annotations=annotations)
-        #self.pixclass.load_model(model_type='resnet50')
-        self.pixclass.load_model(model_type='vgg16')
+        self.data = data.mean(axis=0)
         
-        self.pixclass.compute_multiscale_features()
+        if self.model is None:
+            self.model = Hookmodel(model_name='vgg16')
+            self.model.register_hooks(selected_layers=[list(self.model.module_dict.keys())[0]])
+
+        features, targets = get_features_current_layers(
+            model=self.model, image=self.data, annotations=annotations, scalings=[1,2], order=1, use_min_features=False)
+        self.random_forest = train_classifier(features, targets)
+
 
     def _on_click_ml_mask(self):
 
         if 'annotations' not in self.viewer.layers:
             raise ValueError('No annotation layer found')
         
-        if self.pixclass is None:
+        if self.model is None:
             self._on_initialize_model()
-
-        reduce_fact = 4
-        annotations = self.viewer.layers['annotations'].data[::reduce_fact,::reduce_fact]
-        self.pixclass.annotations = annotations
-        self.pixclass.extract_annotated_features()
-        self.pixclass.train_model()
-        pred = self.pixclass.predict()
+        
+        pred = predict_image(self.data, self.model, self.random_forest, scalings=[1,2], order=1, use_min_features=False)
         pred = (pred == 1).astype(np.uint8)
         predict_upscale = skimage.transform.resize(
             pred, self.viewer.layers['annotations'].data.shape, order=0)
