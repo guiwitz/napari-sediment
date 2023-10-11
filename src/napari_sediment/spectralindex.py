185e1cae1a4551138729bcd5af74d6a511cdceb2
guiwitz <guiwitz@users.noreply.github.com>
2023-10-11 15:49:31 +0200

    index computing out of widget

--- src/napari_sediment/spectralindex.py
+++ src/napari_sediment/spectralindex.py
@@ -1,7 +1,7 @@
 from dataclasses import dataclass, field
-import dataclasses
-from pathlib import Path
-import yaml
+import numpy as np
+
+from .sediproc import find_index_of_band
 
 @dataclass
 class SpectralIndex:
@@ -53,4 +53,46 @@ class SpectralIndex:
         self.left_band = self.left_band_default
         self.right_band = self.right_band_default
         self.middle_band = self.middle_band_default
+
+
+def compute_index_RABD(left, trough, right, row_bounds, col_bounds, imagechannels):
+        """Compute the index RAB.
+        
+        Parameters
+        ----------
+        left: float
+            left band
+        trough: float
+            trough band
+        right: float
+
+        Returns
+        -------
+        RABD: float
+            RABD index
+        """
+
+        ltr = [left, trough, right]
+        # find indices from the end-members plot (in case not all bands were used
+        # This is not necessary as bands will not be skipped in the middle of the spectrum
+        #ltr_endmember_indices = find_index_of_band(self.endmember_bands, ltr)
+        # find band indices in the complete dataset
+        ltr_stack_indices = find_index_of_band(imagechannels.centers,ltr)
+
+        # number of bands between edges and trough
+        #X_left = ltr_endmember_indices[1]-ltr_endmember_indices[0]
+        #X_right = ltr_endmember_indices[2]-ltr_endmember_indices[1]
+        X_left = ltr_stack_indices[1]-ltr_stack_indices[0]
+        X_right = ltr_stack_indices[2]-ltr_stack_indices[1]
+
+        # load the correct bands
+        roi = np.concatenate([row_bounds, col_bounds])
+        ltr_cube = imagechannels.get_image_cube(
+            channels=ltr_stack_indices, roi=roi)
+        ltr_cube = ltr_cube.astype(np.float32)
+
+        # compute indices
+        RABD = ((ltr_cube[0] * X_right + ltr_cube[2] * X_left) / (X_left + X_right)) / ltr_cube[1] 
+
+        return RABD
         
