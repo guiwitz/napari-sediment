a83432589344a8834c799e62b702295b3a9343e8
guiwitz <guiwitz@users.noreply.github.com>
2024-02-02 12:47:57 +0100

    add synthetic data generation, fix tests

--- src/napari_sediment/_tests/test_sediment_widget.py
+++ src/napari_sediment/_tests/test_sediment_widget.py
@@ -1,20 +1,62 @@
 from pathlib import Path
+import numpy as np
 from napari_sediment.sediment_widget import SedimentWidget
+from napari_sediment.data import synthetic
+import os
+from pathlib import Path
+import pytest
+from spectral.io.envi import save_image
 
 
-# make_napari_viewer is a pytest fixture that returns a napari viewer object
-# capsys is a pytest fixture that captures stdout and stderr output streams
-def test_example_q_widget(make_napari_viewer, capsys):
+def create_data():
+
+    if os.path.exists('src/napari_sediment/data/synthetic/Synthetic1_123/capture/Synthetic1_123.hdr'):
+        pass
+    
+    channels = 80
+    im_test, dark_ref, dark_for_white_ref, white_ref = synthetic.generate_synthetic_dataset(
+    image_mean=1000, image_std=5, min_val=300, max_val=400, height=130, width=120, 
+    ref_height=20, channels=channels, white_ref_added_signal=2000, pattern_weight=10, pattern_width=10, random_seed=2)
+
+    im_test = synthetic.add_signal_to_image(im_test=im_test, widths=[15, 30], ch_positions = [40, 40],
+                                            row_boundaries=[[10,20], [60,70]], col_boundaries=[[10,110],[10,110]], amplitudes=[-400, -400], channels=80)
+
+    im_test = synthetic.add_ellipse_to_image(im_test, 100, 37, 10, 20, -600)
+
+    main_path = Path('src/napari_sediment/data/synthetic')
+    os.makedirs(main_path.joinpath('Synthetic1_123/capture').as_posix(), exist_ok=True)
+    os.makedirs(main_path.joinpath('Synthetic1_WR_123/capture').as_posix(), exist_ok=True)
+
+    metadata = {'wavelength': [str(x) for x in np.linspace(300, 900, channels)]}
+    save_image(
+        hdr_file=main_path.joinpath('Synthetic1_123/capture/Synthetic1_123.hdr'),
+        image=im_test, ext='raw', force=True, metadata=metadata)
+    
+    save_image(
+        hdr_file=main_path.joinpath('Synthetic1_123/capture/DARKREF_Synthetic1_123.hdr'),
+        image=dark_ref, ext='raw', force=True, metadata=metadata)
+    
+    save_image(
+        hdr_file=main_path.joinpath('Synthetic1_WR_123/capture/DARKREF_Synthetic1_123.hdr'),
+        image=dark_for_white_ref, ext='raw', force=True, metadata=metadata)
+    save_image(
+        hdr_file=main_path.joinpath('Synthetic1_WR_123/capture/WHITEREF_Synthetic1_123.hdr'),
+        image=white_ref, ext='raw', force=True, metadata=metadata)
+         
+
+def test_select_file(make_napari_viewer, capsys):
     # make viewer and add an image layer using our fixture
+    
+    create_data()
     viewer = make_napari_viewer()
     self = SedimentWidget(viewer)
 
-    imhdr_path = Path('/Users/gw18g940/Desktop/Test_data/Zahajska/synthetic/Synthetic1/Synthetic1_123/capture/Synthetic1_123.hdr')
+    imhdr_path = Path('src/napari_sediment/data/synthetic/Synthetic1_123/capture/Synthetic1_123.hdr')
     self.set_paths(imhdr_path)
     self._on_select_file()
     
     assert 'red' in viewer.layers
     assert 'green' in viewer.layers
     assert 'blue' in viewer.layers
-    assert 'mask' in viewer.layers
+    assert 'imcube' in viewer.layers
     assert len(self.imagechannels.channel_names) == 80, f"Expected 80 channels got {len(self.imagechannels.channel_names)}"
