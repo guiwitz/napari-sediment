1b89ecdb634fe757d1a06cc5d507d5f83c0796b7
guiwitz <guiwitz@users.noreply.github.com>
2024-09-22 22:59:22 +0200

    add workflow tests

--- src/napari_sediment/_tests/test_reader.py
+++ src/napari_sediment/_tests/test_reader.py
@@ -1,14 +1,31 @@
 import numpy as np
+import os
 
-from ..utilities._reader import napari_get_reader
+from napari_sediment.utilities.sediproc import convert_bil_raw_to_zarr
+from napari_sediment.utilities._reader import read_spectral
+from spectral import open_image
+from pathlib import Path
+from dask.array.core import Array
 
+data_folder = Path(os.path.expanduser("~")).joinpath('Sediment_synthetic')
+data_folder = data_folder.joinpath('sediment_data', 'Demo', 'capture/')
 
-# tmp_path is a pytest fixture
-def test_reader(tmp_path):
-    """An example of how you might test your plugin."""
+def test_convert():
+    
+    convert_bil_raw_to_zarr(
+        hdr_path=data_folder.joinpath('Demo.hdr'),
+        export_folder=data_folder)
 
-    pass
+    assert data_folder.joinpath('Demo.zarr').is_dir()
+    assert data_folder.joinpath('Demo.zarr').joinpath('0.0.0').exists()
 
+def test_reader_hdr():
+    data, metadata = read_spectral(data_folder.joinpath('Demo.hdr'), bands=[1,2])
+    assert data.shape == (130,120,2), "Wrong data dimensions"
 
-def test_get_reader_pass():
-    pass
+def test_reader_zarr():
+    data, metadata = read_spectral(data_folder.joinpath('Demo.zarr'), bands=[1,2])
+
+    assert data.shape == (130,120,2), "Wrong data dimensions"
+
+    assert isinstance(data, Array), f"Data type should be dask array but is {type(data)}"
