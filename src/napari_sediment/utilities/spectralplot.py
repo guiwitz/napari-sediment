54f5b680afc75852197b94f8c816886bc11a1112
guiwitz <guiwitz@users.noreply.github.com>
2024-11-25 15:07:37 +0100

    add colorbar to index plot

--- src/napari_sediment/utilities/spectralplot.py
+++ src/napari_sediment/utilities/spectralplot.py
@@ -8,6 +8,8 @@ from napari.utils import colormaps
 from microfilm import colorify
 import tifffile
 import cmap
+from matplotlib import cm
+import matplotlib as mpl
 from ..utilities.io import get_mask_roi, get_rgb_roi, load_plots_params, load_project_params
 from ..utilities.spectralindex_compute import (load_index_series,
                                                compute_and_clean_index, compute_index_projection)
@@ -18,7 +20,203 @@ from ..data_structures.parameters_plots import Paramplot
 def plot_spectral_profile(rgb_image, mask, index_obj, format_dict, scale=1,
                           scale_unit='mm', location="", fig=None, roi=None, left_margin=0,
                           right_margin=0, bottom_margin=0, top_margin=0,
-                          repeat=True):
+                          repeat=True, color_proj_by_index=True, add_colorbar=True):
+    """Given data inputs of RGB image, mask and index, create a figure with RGB image,
+    index map and index projection. Typically called by plot_experiment_roi_index."""
+    
+    if isinstance(index_obj, list):
+        if len(index_obj) > 2:
+            raise ValueError('Only two indices can be plotted at the same time')
+    else:
+        index_obj = [index_obj]
+
+    if len(index_obj) > 1:
+        add_colorbar = False
+    
+    '''for i in range(len(index_obj)):
+        index_name = index_obj[i].index_name
+        index_image = index_obj[i].index_map
+        proj = index_obj[i].index_proj
+        index_contrast_limits = index_obj[i].index_map_range
+        index_colormap = index_obj[i].colormap'''
+
+    im_w = index_obj[0].index_map.shape[1]
+    im_h = index_obj[0].index_map.shape[0]
+
+    title_font = format_dict['title_font']
+    label_font = format_dict['label_font']
+    color_plotline = format_dict['color_plotline']
+    plot_thickness = format_dict['plot_thickness']
+    red_contrast_limits = format_dict['red_contrast_limits']
+    green_contrast_limits = format_dict['green_contrast_limits']
+    blue_contrast_limits = format_dict['blue_contrast_limits']
+
+    # get colormap
+    mlp_colormaps = [Colormap(colormaps.ALL_COLORMAPS[x.colormap].colors).to_matplotlib() for x in index_obj]
+    if index_obj[0].index_map_range is None:
+        rescale_type = 'min_max'
+    else:
+        rescale_type = 'limits'
+    index_image, _, _, _ = colorify.multichannel_to_rgb(
+        images=[x.index_map for x in index_obj],
+        rescale_type=rescale_type,
+        limits=[x.index_map_range for x in index_obj],
+        proj_type='sum',
+        alpha=0.5,
+        cmap_objects=mlp_colormaps,
+        )
+
+    if color_proj_by_index:
+        color_plotline = [colormaps.ALL_COLORMAPS[x.colormap].colors[-1,:] for x in index_obj]
+
+    rgb_to_plot = create_rgb_image(rgb_image, red_contrast_limits, green_contrast_limits, blue_contrast_limits)
+    rgb_to_plot[mask == 1, :] = 0
+
+    if im_h / im_w > 2:
+        a4_size = np.array([11.69, 8.27])
+    else:
+        a4_size = np.array([8.27, 11.69])
+    a4_margins = a4_size - np.array([bottom_margin + top_margin, left_margin + right_margin])
+
+    pixel_in_inches = a4_margins[0] / im_h
+    im_height_inches = a4_margins[0]
+    im_width_inches = im_w * pixel_in_inches
+    plot_width_inches = a4_margins[1] - 2 * im_width_inches
+    if plot_width_inches < 2:
+        im_width_inches_new = (a4_margins[1] - 2) / 2
+        ratio = im_width_inches_new / im_width_inches
+        im_height_inches = im_height_inches * ratio
+        im_width_inches = im_width_inches_new
+        plot_width_inches = 2
+
+    # The figure and axes are set explicitly to make sure that the axes fill the figure
+    # This is achieved using the add_axes method instead of subplots
+    fig_size = [a4_size[1], a4_size[0]]
+    if fig is None:
+        fig = plt.figure()
+    fig.clear()
+    fig.set_size_inches(fig_size)
+    fig.set_facecolor('white')
+    
+    ax1 = fig.add_axes(rect=(left_margin/a4_size[1], bottom_margin/a4_size[0], im_width_inches/a4_size[1], im_height_inches/a4_size[0]))
+    ax2 = fig.add_axes(rect=(im_width_inches/a4_size[1]+left_margin/a4_size[1], bottom_margin/a4_size[0], im_width_inches/a4_size[1], im_height_inches/a4_size[0]))
+    ax3 = fig.add_axes(rect=((2 * im_width_inches+left_margin)/a4_size[1], bottom_margin/a4_size[0], plot_width_inches/a4_size[1], im_height_inches/a4_size[0]))
+    if add_colorbar:
+        #ax2b = fig.add_axes(rect=((2*im_width_inches+left_margin+0.1)/a4_size[1], bottom_margin/a4_size[0],0.8*cbar_frac * plot_width_inches/a4_size[1], 0.5*im_height_inches/a4_size[0]))
+        ax2b = fig.add_axes(rect=(1.1 * im_width_inches/a4_size[1] + left_margin/a4_size[1], bottom_margin/a4_size[0] - 0.05 * im_height_inches/a4_size[0], 0.8 * im_width_inches/a4_size[1], 0.04 * im_height_inches/a4_size[0])) 
+        norm = mpl.colors.Normalize(vmin=index_obj[0].index_map_range[0], vmax=index_obj[0].index_map_range[1])
+        colorbar = fig.colorbar(cm.ScalarMappable(norm=norm, cmap=mlp_colormaps[0]), cax=ax2b, orientation='horizontal')
+
+    ax1.imshow(rgb_to_plot, aspect='auto')
+    '''if index_contrast_limits is None:
+        non_nan = index_image[~np.isnan(index_image)]
+        vmin = np.percentile(non_nan, 0.1)
+        vmax = np.percentile(non_nan, 99.9)
+    else:
+        vmin = index_contrast_limits[0]
+        vmax = index_contrast_limits[1]'''
+    index_image[mask==1,:] = np.nan
+    ax2.imshow(index_image, aspect='auto', interpolation='none')
+
+    if roi is not None:
+        roi_array = np.array(roi)
+        roi_array[1,0] -=0.5
+        roi_array[2,0] -=0.5
+        roi_array[0,0] -=0.5
+        roi_array[3,0] -=0.5
+        roi_array = np.concatenate([roi_array, roi_array[[0]]])
+        ax2.plot(roi_array[:,1], roi_array[:,0], 'r')
+    
+    proj = [x.index_proj for x in index_obj]
+    for ind, p in enumerate(proj):
+        ax3.plot(p, np.arange(len(p)), color=np.array(color_plotline[ind]), linewidth=plot_thickness)
+        ax3.plot(np.ones_like(p) * np.nanmean(p), np.arange(len(p)), color='black', linestyle='--')
+
+    ax3.set_ylim(0, len(proj))
+    ax3.yaxis.tick_right()
+    ax3.invert_yaxis()
+    
+    # set y axis scale
+    for ax in [ax1]:
+        tickpos = np.array([x.get_position()[1] for x in  ax.get_yticklabels()])[1:-1]
+        new_labels = scale * np.array(tickpos)
+        tickdist = (new_labels[-1] - new_labels[0]) / 10
+        order_of_mag = 10 ** int(np.floor(np.log10(tickdist)))
+        tickdist = order_of_mag * (tickdist // order_of_mag)
+        new_labels = np.arange(0, new_labels[-1] + 5 * tickdist, tickdist)
+        new_labels = new_labels[new_labels <= im_h * scale]
+        new_tickpos = new_labels / scale
+        ax.set_yticks(ticks=new_tickpos, labels=new_labels)
+    ax3.set_yticks(ticks=new_tickpos, labels=new_labels)
+
+    ax1.set_xticks([])
+    ax2.set_xticks([])
+    ax2.set_yticks([])
+    for label in (ax1.get_yticklabels() + ax3.get_yticklabels() + ax3.get_xticklabels()):
+        label.set_fontsize(label_font)
+    
+    ax2.set_ylim(im_h - 0.5, -0.5)
+    ax1.set_ylabel(f'depth [{scale_unit}]', fontsize=label_font)
+    ax3.set_ylabel(f'depth [{scale_unit}]', fontsize=label_font)
+    ax3.set_xlabel('Index value', fontsize=label_font)
+    ax3.yaxis.set_label_position('right')
+    full_name = ", ".join([x.index_name for x in index_obj])
+    suptitle = fig.suptitle(full_name + '\n' + location,
+                    fontsize=title_font)
+    
+    # check the size of titles labels and tickmarks, adjust margins accordingly,
+    # and repeat the plot
+
+    # adjust left margin
+    renderer = fig.canvas.get_renderer()
+    text = ax1.yaxis.label
+    label_width = get_text_width(text, renderer, fig)
+    y_tick_widths = [get_text_width(label, renderer, fig) for label in ax1.get_yticklabels()]
+    max_y_tick_width = max(y_tick_widths)
+    left_margin = 1.0 * (3 * label_width + max_y_tick_width) * a4_size[1]
+
+    # adjust right margin
+    text = ax3.yaxis.label
+    label_width = get_text_width(text, renderer, fig)
+    y_tick_widths = [get_text_width(label, renderer, fig) for label in ax3.get_yticklabels()]
+    max_y_tick_width = max(y_tick_widths)
+    right_margin = 1.0 * (3 * label_width + max_y_tick_width) * a4_size[1]
+
+    # adjust bottom margin
+    text = ax3.xaxis.label
+    label_height = get_text_height(text, renderer, fig)
+    x_tick_heights = [get_text_height(label, renderer, fig) for label in ax3.get_xticklabels()]
+    max_x_tick_height = max(x_tick_heights)
+    bottom_margin = 1.0 * (3 * label_height + max_x_tick_height) * a4_size[0]
+
+    if add_colorbar:
+        cbar_box = colorbar.ax.get_position()
+        texts = ax2b.get_xticklabels()
+        bbox = texts[0].get_window_extent(renderer)
+        bbox = bbox.transformed(fig.transFigure.inverted())
+        cbar_ysize = (cbar_box.ymax - cbar_box.ymin) + bbox.ymax-bbox.ymin
+        bottom_margin = bottom_margin + cbar_ysize
+
+    # adjust top margin
+    bbox = suptitle.get_window_extent(renderer).transformed(fig.transFigure.inverted())
+    title_height = bbox.ymax - bbox.ymin
+    top_margin = 2 * title_height * a4_size[0]
+
+    if repeat:
+        # remake plot with adjusted margins adapted to content 
+        plot_spectral_profile(rgb_image, mask, index_obj, format_dict, scale=scale,
+                          scale_unit=scale_unit, location=location, fig=fig, roi=roi,
+                          left_margin=left_margin, right_margin=right_margin,
+                          bottom_margin=bottom_margin, top_margin=top_margin,
+                          repeat=False, color_proj_by_index=color_proj_by_index, add_colorbar=add_colorbar)
+
+
+    return fig, ax1, ax2, ax3
+
+def plot_spectral_profile_original(rgb_image, mask, index_obj, format_dict, scale=1,
+                          scale_unit='mm', location="", fig=None, roi=None, left_margin=0,
+                          right_margin=0, bottom_margin=0, top_margin=0,
+                          repeat=True, color_proj_by_index=True):
     """Given data inputs of RGB image, mask and index, create a figure with RGB image,
     index map and index projection. Typically called by plot_experiment_roi_index."""
     
@@ -43,7 +241,7 @@ def plot_spectral_profile(rgb_image, mask, index_obj, format_dict, scale=1,
     newmap = Colormap(colormaps.ALL_COLORMAPS[index_colormap].colors)
     mpl_map = newmap.to_matplotlib()
 
-    if color_plotline == []:
+    if color_proj_by_index:
         color_plotline = colormaps.ALL_COLORMAPS[index_colormap].colors[-1,:]
 
     rgb_to_plot = create_rgb_image(rgb_image, red_contrast_limits, green_contrast_limits, blue_contrast_limits)
@@ -169,7 +367,7 @@ def plot_spectral_profile(rgb_image, mask, index_obj, format_dict, scale=1,
                           scale_unit=scale_unit, location=location, fig=fig, roi=roi,
                           left_margin=left_margin, right_margin=right_margin,
                           bottom_margin=bottom_margin, top_margin=top_margin,
-                          repeat=False)
+                          repeat=False, color_proj_by_index=color_proj_by_index)
 
 
     return fig, ax1, ax2, ax3
@@ -190,7 +388,7 @@ def get_text_height(text, renderer, fig):
 def plot_multi_spectral_profile(rgb_image, mask, index_objs, format_dict, scale=1,
                                 scale_unit='mm', location="", fig=None, roi=None,
                                 left_margin=0, right_margin=0, bottom_margin=0,
-                                top_margin=0, repeat=True):
+                                top_margin=0, repeat=True, color_proj_by_index=True):
     
     """
     Given data inputs of RGB image, mask and indices, create a figure of
@@ -252,7 +450,7 @@ def plot_multi_spectral_profile(rgb_image, mask, index_objs, format_dict, scale=
         index_name = index_objs[i].index_name
 
         # get line color from colormap
-        if color_plotline == []:
+        if color_proj_by_index:
             current_color = colormaps.ALL_COLORMAPS[index_objs[i].colormap].colors[-1,:]
         else:
             current_color = np.array(color_plotline)
@@ -364,7 +562,7 @@ def plot_multi_spectral_profile(rgb_image, mask, index_objs, format_dict, scale=
                                 scale_unit=scale_unit, location=location, fig=fig,
                                 roi=roi, left_margin=left_margin,
                                 right_margin=right_margin, bottom_margin=bottom_margin,
-                                top_margin=top_margin, repeat=False)
+                                top_margin=top_margin, repeat=False, color_proj_by_index=color_proj_by_index)
 
     return fig
 
@@ -381,7 +579,7 @@ def create_rgb_image(rgb_image, red_contrast_limits, green_contrast_limits, blue
     return rgb_to_plot
 
 def plot_experiment_roi_index(export_folder, index, params_plots=None, main_roi_index=0,
-                    mask=None, rgb_cube=None, myimage=None):
+                    mask=None, rgb_cube=None, myimage=None, add_colorbar=True):
     """Plot spectral index for a given experiment and region of interest. Data
     such as the mask and rgb image are loaded from the export folder, but can also
     be provided as arguments to avoid repeated loading of the same data. This function
@@ -404,7 +602,9 @@ def plot_experiment_roi_index(export_folder, index, params_plots=None, main_roi_
         rgb image for the roi
     myimage : object
         ImChannels object with image data
-    
+    add_colorbar : bool
+        whether to add a colorbar to the index plot
+
     Returns
     -------
     fig : matplotlib.figure.Figure
@@ -440,20 +640,27 @@ def plot_experiment_roi_index(export_folder, index, params_plots=None, main_roi_
         params_plots.blue_contrast_limits = [min_contrast[2], max_contrast[2]]
 
     # compute index map and projection
-    index.index_map = compute_and_clean_index(
-            spectral_index=index, row_bounds=row_bounds,
-            col_bounds=col_bounds, imagechannels=myimage)
-            
-    index.index_proj = compute_index_projection(
-                index.index_map, mask,
-                colmin=colmin_proj, colmax=colmax_proj,
-                smooth_window=index.smooth_proj_window)
+    if isinstance(index, list):
+        if len(index) > 2:
+            raise ValueError('Only one or two indices can be plotted at a time')
+    else:
+        index = [index]
     
+    for ind in index:
+        ind.index_map = compute_and_clean_index(
+                spectral_index=ind, row_bounds=row_bounds,
+                col_bounds=col_bounds, imagechannels=myimage)
+                
+        ind.index_proj = compute_index_projection(
+                    ind.index_map, mask,
+                    colmin=colmin_proj, colmax=colmax_proj,
+                    smooth_window=ind.smooth_proj_window)
+        
     fig, ax1, ax2, ax3 = plot_spectral_profile(
         rgb_image=rgb_cube, mask=mask, index_obj=index,
         format_dict=format_dict, scale=params.scale, scale_unit=params.scale_units,
         location=params.location, fig=None, 
-        roi=measurement_roi, repeat=True)
+        roi=measurement_roi, repeat=True, add_colorbar=add_colorbar)
     
     return fig, mask, rgb_cube, myimage
 
