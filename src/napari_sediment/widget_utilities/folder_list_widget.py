ada31c15807dd30e86e7b3029fd4e85b9a3e0831
guiwitz <guiwitz@users.noreply.github.com>
2024-12-07 15:11:18 +0100

    more ignore options

--- src/napari_sediment/widget_utilities/folder_list_widget.py
+++ src/napari_sediment/widget_utilities/folder_list_widget.py
@@ -6,11 +6,30 @@ from qtpy.QtCore import Qt
 
 class FolderListWidget(QListWidget):
     # be able to pass the Napari viewer name (viewer)
-    def __init__(self, viewer, parent=None, background_kw='_WR_'):
+    def __init__(self, viewer, parent=None, background_kw='_WR_', only_folders=False, ignore=None):
+        """List widget to show the files in a folder.
+        
+        Parameters
+        ----------
+        viewer : str
+            Napari viewer name.
+        parent : QWidget, optional
+            Parent widget.
+        background_kw : str, optional
+            Keyword to ignore files containing this name.
+        only_folders : bool, optional
+            Only show folders.
+        ignore : list of str, optional
+            List of file names to ignore.
+        
+        """
+
         super().__init__(parent)
 
         self.viewer = viewer
         self.background_kw = background_kw
+        self.only_folders = only_folders
+        self.ignore = ignore
         self.setAcceptDrops(True)
         self.setDragEnabled(True)
         #self.currentItemChanged.connect(self.open_file)
@@ -45,14 +64,20 @@ class FolderListWidget(QListWidget):
                         self.addItem(f)
 
     def update_from_path(self, path):
-
+        
+        if self.ignore is None:
+            self.ignore = []
         self.clear()
         self.folder_path = path
         files = os.listdir(self.folder_path)  
         for f in files:
             #if Path(f).suffix == '.oir':
-            if (f[0] != '.') and (self.background_kw not in f):
-                self.addItem(f)
+            if (f[0] != '.') and (self.background_kw not in f) and (f not in self.ignore):
+                if self.only_folders:
+                    if os.path.isdir(Path(self.folder_path).joinpath(f)):
+                        self.addItem(f)
+                else:
+                    self.addItem(f)
 
     def add_elements(self, elements):
 
